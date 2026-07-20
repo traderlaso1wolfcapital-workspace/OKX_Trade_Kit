87db184528302c83731c0dc90979d6b0b3d35c1a
traderlaso1wolfcapital-creator <traderlaso1.wolfcapital@gmail.com>
2026-07-21 01:11:03 +0700

    Clean up old local build scripts and artifacts

--- TLS1_Trading_App/test_bat.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python -c "import re, json; v=json.load(open('version.json', encoding='utf-8'))['version']; content=open('gui_main.py', encoding='utf-8').read(); content=re.sub(r'^APP_VERSION = .*$', f'APP_VERSION = \"{v}\"', content, flags=re.MULTILINE); open('gui_main.py', 'w', encoding='utf-8').write(content); print(' -> Da hardcode APP_VERSION = \"' + v + '\" vao gui_main.py')"
